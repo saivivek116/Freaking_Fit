f3a8b9d2001956222dc96c6ac01f3ce1c53ac76d
Shreyas Kv <logsinx96@gmail.com>
2023-10-31 13:09:06 +0000

    changed community page

--- src/components/CommunityCard.js
+++ src/components/CommunityCard.js
@@ -34,25 +34,25 @@ function CommunityCard() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
-                            src="images/weight.webp"
-                            text="Welcome to the Weightlifting Warriors community! Whether you are a seasoned lifter or just starting out, this is the place for you. Share your favorite lifting techniques, discuss workout plans, and track your progress with fellow warriors."
+                            src="images/weight.jpg"
+                            text="Lifters, share your favorite lifting techniques, discuss workout plans, and track your progress with fellow warriors."
                             label="Weightlifting Warriors"
                         />
                         <CardItem
-                            src="images/HIIT.webp"
-                            text="Welcome to HIIT Heroes! High-Intensity Interval Training is the name of the game here. Join us to discover intense workouts, exchange HIIT routines, and get inspired to push your limits."
+                            src="images/HIIT.jpg"
+                            text="High-Intensity Interval Training! Discover intense workouts, exchange routines, and get inspired to push your limits."
                             label="HIIT Heroes"
                         />
                     </ul>
                     <ul className="cards__items">
                         <CardItem
-                            src="images/cardio.webp"
-                            text="Cardio enthusiasts, this is the place for you! Share your favorite running routes, cycling tips, and more.. Cardio community to perform activities such as treadmill and cycling"
+                            src="images/cardio.jpg"
+                            text="Cardio enthusiasts, this is the place for you! Share your favorite running routes, cycling tips, and more!"
                             label="Cardio Crew"
                         />
                         <CardItem
-                            src="images/yoga.webp"
-                            text="Welcome to the Yoga Enthusiasts community! Find your inner peace and flexibility with us. Share yoga poses, meditation techniques, and connect with fellow yogis on your journey to mindfulness."
+                            src="images/yoga.jpg"
+                            text="Find your inner peace and flexibility with us. Share yoga poses, meditation techniques, and connect with fellow yogis."
                             label="Yoga Enthusiasts"
                         />
                     </ul>
