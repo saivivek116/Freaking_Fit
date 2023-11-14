8c09abe82441d779a04f3eaf9bebf4bc5971e1bc
athunavda <athu.navada@gmail.com>
2023-11-13 19:29:13 -0500

    fixed: zones made it what it supposed to be fixed: footer design fixed: navbar in mobile view fixed: padding issue in workout section fixed: inconsistent font fixed: adding logo to navbar

--- src/pages/Diet.js
+++ src/pages/Diet.js
@@ -1,79 +1,58 @@
 import React from "react";
-import "./diet.scss";
-
-const dietPlans = [
-    {
-        id: 1,
-        name: "Keto Diet",
-        imageSrc: "keto.webp",
-        description:
-            "The Keto Diet is a low-carb, high-fat diet that has been shown to help people lose weight. Join our Keto community to share recipes, meal plans, and success stories.",
-    },
-    {
-        id: 2,
-        name: "Paleo Diet",
-        imageSrc: "paleo.webp",
-        description:
-            "The Paleo Diet focuses on eating foods that our ancestors would have consumed, such as lean meats, fish, fruits, and vegetables. Connect with fellow Paleolithic eaters and discover new recipes.",
-    },
-    {
-        id: 3,
-        name: "Vegan Diet",
-        imageSrc: "vegan.webp",
-        description:
-            "Vegans abstain from all animal products. If you follow a vegan lifestyle or are interested in transitioning, join our Vegan community for support, recipes, and tips.",
-    },
-    {
-        id: 4,
-        name: "Mediterranean Diet",
-        imageSrc: "mediterranean.webp",
-        description:
-            "The Mediterranean Diet emphasizes whole grains, fruits, vegetables, and healthy fats. Join our Mediterranean community to learn about this heart-healthy way of eating.",
-    },
-    {
-        id: 5,
-        name: "Intermittent Fasting",
-        imageSrc: "intermittent.webp",
-        description:
-            "Intermittent Fasting involves cycling between periods of fasting and eating. Learn about different fasting protocols and share your experiences with our community.",
-    },
-    {
-        id: 6,
-        name: "Vegetarian Diet",
-        imageSrc: "vegetarian.webp",
-        description:
-            "The Vegetarian Diet excludes meat but includes plant-based foods. Connect with fellow vegetarians and explore a variety of meatless recipes and meal plans.",
-    },
-    // Add more diet plans as needed
-];
-
-function DietItem({ name, imageSrc, description }) {
-    return (
-        <li className="diet-plan-item">
-            {/* <div className="diet-image"> */}
-            <img src={`images/${imageSrc}`} alt={name} />
-            {/* </div> */}
-            <div className="diet-plan-info">
-                <h3>{name}</h3>
-                <p>{description}</p>
-            </div>
-        </li>
-    );
-}
+import DietItem from "./DietItem";
 
 function Diet() {
-    return (
-        <div className="diet-page pt-5">
-            <section id="diet">
-                <h1>Discover Your Path to Wellness: Explore Our Diet Plans</h1>
-                <div className="diet-plan-list">
-                    {dietPlans.map((dietPlan) => (
-                        <DietItem key={dietPlan.id} {...dietPlan} />
-                    ))}
-                </div>
-            </section>
-        </div>
-    );
+	return (
+		<>
+			<div className="cards">
+				<h1>Discover Your Path to Wellness: Explore Our Diet Plans</h1>
+				<div className="cards__container">
+					<div className="cards__wrapper">
+						<ul className="cards__items">
+							<DietItem
+								path="/keto"
+								src="images/keto.webp"
+								text="The Keto Diet is a low-carb, high-fat diet that has been shown to help people lose weight. Join our Keto community to share recipes, meal plans, and success stories."
+								label="Keto Diet"
+							/>
+							<DietItem
+								path="/paleo"
+								src="images/paleo.webp"
+								text="The Paleo Diet focuses on eating foods that our ancestors would have consumed, such as lean meats, fish, fruits, and vegetables. Connect with fellow Paleolithic eaters and discover new recipes."
+								label="Paleo Diet"
+							/>
+							<DietItem
+								path="/vegan"
+								src="images/vegan.webp"
+								text="Vegans abstain from all animal products. If you follow a vegan lifestyle or are interested in transitioning, join our Vegan community for support, recipes, and tips."
+								label="Vegan Diet"
+							/>
+						</ul>
+						<ul className="cards__items">
+							<DietItem
+								path="/mediterranean"
+								src="images/mediterranean.webp"
+								text="The Mediterranean Diet emphasizes whole grains, fruits, vegetables, and healthy fats. Join our Mediterranean community to learn about this heart-healthy way of eating."
+								label="Mediterranean Diet"
+							/>
+							<DietItem
+								path="/intermittent"
+								src="images/intermittent.webp"
+								text="Intermittent Fasting involves cycling between periods of fasting and eating. Learn about different fasting protocols and share your experiences with our community."
+								label="Intermittent Diet"
+							/>
+							<DietItem
+								path="/vegetarian"
+								src="images/vegetarian.webp"
+								text="The Vegetarian Diet excludes meat but includes plant-based foods. Connect with fellow vegetarians and explore a variety of meatless recipes and meal plans."
+								label="Vegetarian Diet"
+							/>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Diet;
