9a206708d7c497ae9a1f2d8c60b260ac108c7e0e
saivivek mac <saivivek116@gmail.com>
2023-11-21 11:31:29 -0500

    changes

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,12 +4,15 @@ import "./notfound.scss";
 
 const NotFound = () => {
     return (
-        <>
-            <h2 className="notfound">
-                404 Page Not Found!
-                <Link to={"/"}>Home</Link>
-            </h2>
-        </>
+        <div className="notfound">
+            <h3>OOPS! The page you are looking for isn't available.</h3>
+            <br />
+            <p>It might be under development or have been removed.</p>
+            <p>We're sorry for any inconvenience!"</p>
+            Please go back to
+            <br />
+            <Link to={"/"}>Home</Link>
+        </div>
     );
 };
 
